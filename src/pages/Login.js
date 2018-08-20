424a2ce725c39d899b5fff77894f79bee796ff41
ByronGBP <byrongbp@gmail.com>
2018-08-20 18:14:17 +0200

    add form component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,12 @@
 // @flow
 import React from 'react';
 
+import Form from '../components/forms/Login';
+
 const Login = () => (
-  <div>
-    Login!
-  </div>
+  <Form title="Login" handleSubmit={(fn) => {fn();}}submit={() => {return 'damn';}}>
+      HI!
+  </Form>
 );
 
 export default Login;

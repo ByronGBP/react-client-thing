aea516075f7a76044a78351d1c3d475109848fe6
ByronGBP <byrongbp@gmail.com>
2018-08-20 15:47:49 +0200

    add notfound page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+// @flow
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    404 - Not found!
+  </div>
+);
+
+export default NotFound;
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 
 import Login from './Login';
+import NotFound from './NotFound';
 
 const Root = () => (
   <Router>
     <Switch>
       <Route path="/login" component={Login} />
+      <Route component={NotFound} />
     </Switch>
   </Router>
 );

352dccc2fe87f016b93de70e4f0484d003ebbad3
ByronGBP <byrongbp@gmail.com>
2018-08-21 22:28:58 +0200

    add news prop

--- src/components/news/NewsItem.js
+++ src/components/news/NewsItem.js
@@ -1,16 +1,22 @@
 // @flow
 import React from 'react';
 
-const NewsItem = () => (
+import type { News } from '../../types/api';
+
+type Props = {
+  news: News
+}
+
+const NewsItem = ({ news }: Props) => (
   <div className="list-group-item list-group-item-action flex-column align-items-start nvx-hover-click">
     <div className="d-flex">
       <div className="d-flex align-items-center nvx-img">
-        <img className="img-fluid" src="https://htmlcolors.com/img/change-user.png" alt="fw-default-avatar" />
+        <img className="img-fluid" src={news.avatar} alt="fw-default-avatar" />
       </div>
       <div className="d-flex align-items-center w-100">
         <div className="d-flex w-100 justify-content-between">
-          <h5 className="mb-1">List group item heading</h5>
-          <small>3 days ago</small>
+          <h5 className="mb-1">{news.title}</h5>
+          <small>{news.date}</small>
         </div>
       </div>
     </div>
--- src/components/news/NewsList.js
+++ src/components/news/NewsList.js
@@ -3,12 +3,19 @@ import React from 'react';
 
 import NewsItem from './NewsItem';
 
-const NewsList = () => (
+import type { News } from '../../types/api';
+
+type Props = {
+  news: [News]
+}
+
+const NewsList = ({ news }: Props) => (
   <div className="d-flex justify-content-center nvx-space">
     <div className="list-group w-75">
-      <NewsItem />
-      <NewsItem />
-      <NewsItem />
+      {news.map((elem) => {
+        return <NewsItem key={elem._id} news={elem}/>;
+      })}
+      
     </div>
   </div>
 );
